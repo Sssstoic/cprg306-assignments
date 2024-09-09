1eb212ac3472548e5f0f42d5ab2ab5fb1b67ef10
Sssstoic <jamesromar.tuling@edu.sait.ca>
2024-09-09 09:52:27 -0600

    Complete Week 2 demo

--- app/page.js
+++ app/page.js
@@ -1,5 +1,14 @@
+import Link from "next/link";
+
 export default function Page(){
   return (
-    <main>Hello World!</main>
+    <main>
+      <h1>Web Dev 2 Assignments</h1>
+      <ul>
+        <li>
+          <Link href="week-2">Week 2</Link>
+        </li>
+      </ul>
+    </main>
   )
 }
